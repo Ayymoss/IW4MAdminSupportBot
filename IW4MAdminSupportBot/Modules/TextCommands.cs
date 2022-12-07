0c0d8143c7419e07dc7db85656f507a9a9ad5743
Amos <amos2580@hotmail.co.uk>
2022-12-07 22:07:18 +0000

    Basic Discord Bot Setup

---
+++ IW4MAdminSupportBot/Modules/TextCommands.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+
+namespace IW4MAdminSupportBot.Modules;
+
+public class TextCommands : ModuleBase<SocketCommandContext>
+{
+    [Command("ping")]
+    public async Task PingAsync() => await ReplyAsync("pong!");
+
+    [Command("faq")]
+    public async Task CatAsync() => await ReplyAsync("pong!");
+}
---
+++ IW4MAdminSupportBot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using IW4MAdminSupportBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IW4MAdminSupportBot;
+
+// Credit: https://github.com/discord-net/Discord.Net/tree/dev/samples
+internal static class Program
+{
+    private static void Main() => MainAsync().GetAwaiter().GetResult();
+
+    private static async Task MainAsync()
+    {
+        await using var services = ConfigureServices();
+        var client = services.GetRequiredService<DiscordSocketClient>();
+
+        client.Log += LogAsync;
+        services.GetRequiredService<CommandService>().Log += LogAsync;
+
+        var fileJson = await File.ReadAllTextAsync("BotConfiguration.json");
+        var token = JsonSerializer.Deserialize<BotConfiguration>(fileJson);
+
+        await client.LoginAsync(TokenType.Bot, token?.Token);
+        await client.StartAsync();
+        await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private static async Task LogAsync(LogMessage log) => Console.WriteLine(log.ToString());
+
+    private static ServiceProvider ConfigureServices()
+    {
+        return new ServiceCollection()
+            .AddSingleton(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            })
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton<CommandService>()
+            .AddSingleton<CommandHandlingService>()
+            .AddSingleton<HttpClient>()
+            .BuildServiceProvider();
+    }
+}
+
+public class BotConfiguration
+{
+    public string Token { get; set; } = null!;
+}
